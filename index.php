cb9f8e745da1329ce0769244deaa6a4e743546e3
James Bouska <jbouska419@gmail.com>
2014-08-05 17:56:26 -0500

    installed bower and add lesswatch

---
+++ index.php
@@ -0,0 +1,24 @@
+<?php 
+defined('_JEXEC') or die; 
+?>
+<!DOCTYPE html>
+	<html lang="en">
+		<head>
+			<meta charset="utf-8">
+			<meta http-equiv="X-UA-Compatible" content="IE=edge">
+			<meta name="viewport" content="width=device-width, initial-scale=1">
+			<jdoc:include type="head" />
+			<!-- Load Single CSS File -->
+			<link rel="stylesheet" href="<?php echo $this->baseurl ?>/templates/<?php echo $this->template; ?>/css/load.css">
+			<!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+			<!--[if lt IE 9]>
+				<script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+				<script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+			<![endif]-->
+		</head>
+		<body>
+                	<jdoc:include type="modules" name="menu" />
+                	<jdoc:include type="message" />
+					<jdoc:include type="component" />
+		</body>
+</html>
