b323eda775d0db1242c4e39e85809dc3a8b8b851
antfuentes87 <antfuentes87@gmail.com>
2014-08-07 12:57:15 -0500

    remove comments

--- html/com_content/article/default_links.php
+++ html/com_content/article/default_links.php
@@ -1,12 +1,4 @@
 <?php
-/**
- * @package     Joomla.Site
- * @subpackage  com_content
- *
- * @copyright   Copyright (C) 2005 - 2014 Open Source Matters, Inc. All rights reserved.
- * @license     GNU General Public License version 2 or later; see LICENSE.txt
- */
-
 defined('_JEXEC') or die;
 
 // Create shortcut
