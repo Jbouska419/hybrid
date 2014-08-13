5ae8d8e6e0fd26da4bebba3c2840d881c116cd71
antfuentes87 <antfuentes87@gmail.com>
2014-08-13 17:26:24 -0500

    Bower update and created a head,php in src

---
+++ include/src/head.php
@@ -0,0 +1,10 @@
+<meta charset="utf-8">
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<jdoc:include type="head" />
+<!-- Load Single CSS File -->
+<link rel="stylesheet" href="<?php echo $this->baseurl ?>/templates/<?php echo $this->template; ?>/css/load.css">
+<!--[if lt IE 9]>
+<script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+<script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+<![endif]-->
